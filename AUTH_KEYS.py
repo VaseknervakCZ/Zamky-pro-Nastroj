2756338acb64700e200cfd29c4164ff69c228998
VaseknervakCZ <vendelinkovar1908@gmail.com>
2026-03-07 17:23:46 +0100

    v

---
+++ AUTH_KEYS.py
@@ -0,0 +1,10 @@
+AUTH_KEYS = {
+    "Test": "Test-KEY-847",
+    "VaseknervakCZ": "Owner-10-450",
+    "Vasulda": "STEAM-KEY-Frajeri-V",
+    "Skull58_czsk": "STEAM-KEY-Frajeri-S",
+    "GhostKiller_cz": "STEAM-KEY-Frajeri-G",
+    "PavelSeda127": "STEAM-KEY-PAVEL-127",
+    "": "STEAM-KEY-GITHUB-USER-8746-1452s",
+    # Přidej další: "Jmeno": "KLIC-KTERY-MU-DAS",
+}
